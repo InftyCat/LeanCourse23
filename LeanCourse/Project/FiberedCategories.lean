e69063bb9d312d7e6112f4a50125c1220559057c
Tim Lichtnau <tlichtnau@gmail.com>
2023-12-02 10:37:59 +0100

    Added CartesianFunctor between fibrations

--- LeanCourse/Project/FiberedCategories.lean
+++ LeanCourse/Project/FiberedCategories.lean
@@ -1,16 +1,16 @@
 
 import Mathlib.CategoryTheory.Functor.Currying
 import Mathlib.CategoryTheory.Products.Basic
-
+import Mathlib.CategoryTheory.Over
 set_option autoImplicit true
 
 namespace CategoryTheory
 
 open Opposite
 
-universe v₁ v₂ u₁ u₂
+universe v₁ u₁ --v₂ u₁ u₂
 -- morphism levels before object levels. See note [CategoryTheory universes].
-variable {𝕏 : Type u₂} {B : Type u₁} [Category.{v₁} B] [Category.{v₂} 𝕏] {P : 𝕏 ⥤ B}
+variable {𝕏 : Type u₁} {B : Type u₁} [Category.{v₁} B] [Category.{v₁} 𝕏] {P : 𝕏 ⥤ B}
 namespace FiberedCategories
 
 def comp {X Y Z : B}  : (X ⟶ Y) → (Y ⟶ Z ) → (X ⟶ Z) := fun f g => f ≫ g
@@ -22,12 +22,14 @@ def substDom {X Y Z : B} (h : X = Y) (f : Y ⟶ Z) : (X ⟶ Z) := CategoryTheory
 
 
 def obj_over (A : B) := {X : 𝕏 // P.obj X = A}
-instance : CoeSort (obj_over (P:=P) A) 𝕏 := ⟨fun α ↦ α.1⟩
-def over_hom {A A' : B} (u : A ⟶ A') (X : obj_over (P:=P) A) (X' : obj_over (P:=P) A') := {α : X.1 ⟶ X'.1 //
+instance : CoeOut (obj_over (P:=P) A) 𝕏 := ⟨fun α ↦ α.1⟩
+def isVertical {X X' : obj_over (P:=P) A} (α : X.1 ⟶ X') := P.map α ≫ CategoryTheory.eqToHom X'.2  = CategoryTheory.eqToHom X.2
+def over_hom {A A' : B} (u : A ⟶ A') (X : obj_over (P:=P) A) (X' : obj_over (P:=P) A') :=
+  {α : X.1 ⟶ X' //
    P.map α ≫ CategoryTheory.eqToHom X'.2  = CategoryTheory.eqToHom X.2 ≫ u }
 
 
-instance Fib : Category (obj_over ( P:= P) A) where
+instance : Category (obj_over ( P:= P) A) where
   Hom ( X X' : obj_over A) := over_hom (𝟙 A) X X' -- { α : verticalOver A // α.X = X ∧ α.X' = X' }
   id (X : obj_over A) := ⟨ 𝟙 X.1 , by
     rw [@Functor.map_id]
@@ -41,33 +43,46 @@ instance Fib : Category (obj_over ( P:= P) A) where
     rw [f.2]
     aesop_cat
      ⟩
+     --check axioms
+
 structure liftOfAlong {J I : B} ( X : obj_over (P:=P) I) (u : J ⟶ I)  where
   Y : obj_over (P:=P) J
   φ : over_hom u Y X
-def isCartesian {J I : B} {u : J ⟶ I} {X : obj_over (P:=P) I} (τ: liftOfAlong X u):=
+instance : CoeDep (liftOfAlong (P:=P) X u) L (L.Y.1 ⟶ X) where
+  coe := L.φ.1
+variable {J I : B} {u : J ⟶ I}
+
+def morphismToLift {X Y : 𝕏} (φ : Y ⟶ X) : liftOfAlong ⟨ X , rfl⟩  (P.map φ) where
+  Y := ⟨ Y , rfl⟩
+  φ := by use φ; simp
+
+
+
+def isCartesian  {X : obj_over (P:=P) I} (τ: liftOfAlong X u):=
   ∀ {K : B} (v : K ⟶ J) (L: liftOfAlong X (v ≫u )) ,
-    ∃! ψ : over_hom v L.Y τ.Y , ψ.1 ≫ τ.2.1 = L.φ.1
-def isWeakCartesian {J I : B} {u : J ⟶ I} {X : obj_over (P:=P) I} (τ: liftOfAlong X u):= ∀ (L : liftOfAlong X u) ,
-  ∃! ψ : L.Y ⟶ τ.Y , ψ.1 ≫ τ.2.1 = L.φ.1
+    ∃! ψ : over_hom v L.Y τ.Y , ψ.1 ≫ τ.φ.1 = L
+
+def isWeakCartesian {X : obj_over (P:=P) I} (τ: liftOfAlong X u):= ∀ (L : liftOfAlong X u) ,
+  ∃! ψ : L.Y ⟶ τ.Y , ψ.1 ≫ τ.φ.1 = L
 
 
+structure cartesianLiftOfAlong {J I : B} ( X : obj_over (P:=P) I) (u : J ⟶ I) extends liftOfAlong X u where
+ isCart : isCartesian (P:=P) L
 
-def cartesianLiftOfAlong {J I : B} ( X : obj_over (P:=P) I) (u : J ⟶ I) := {L : liftOfAlong X u // isCartesian L }
+instance : CoeOut (cartesianLiftOfAlong (P:=P) A u) (liftOfAlong (P:=P) A u) := ⟨fun α ↦ α.1⟩
+
 def transLift {K J I : B} {v : K ⟶ J } {u : J ⟶ I} {X : obj_over I}
   (α : liftOfAlong X u)
   (β : liftOfAlong (α.Y) v )
   : liftOfAlong ( P:=P) X (v ≫ u ) where
   Y := β.Y
-  φ := ⟨ β.φ.1 ≫ α.φ.1 , by rw [@Functor.map_comp, Category.assoc , α.φ.2 , ← Category.assoc , β.φ.2] ; aesop_cat ⟩
+  φ := ⟨ β.φ.1 ≫ α , by rw [@Functor.map_comp, Category.assoc , α.φ.2 , ← Category.assoc , β.φ.2] ; aesop_cat ⟩
 
-def weakCartifCartesian {J I : B} {u : J ⟶ I} {X : obj_over (P:=P) I} {τ: liftOfAlong X u} (isCart : isCartesian τ) : isWeakCartesian τ := by
+def weakCartifCartesian {J I : B} {u : J ⟶ I} {X : obj_over (P:=P) I} (τ: cartesianLiftOfAlong X u) : isWeakCartesian τ.1 := by
   intro L
-  -- obtain ⟨Y , ϕ⟩ := L
-
+  --obtain ⟨ τ , isCart ⟩:= τ
   let τ' : liftOfAlong X (𝟙 J ≫ u) := transLift L (⟨ L.Y , 𝟙 (L.Y)  ⟩  )
-
-  specialize isCart (𝟙 J) τ'
-  obtain ⟨ψ, hψ ⟩:= isCart
+  obtain ⟨ψ, hψ ⟩:= τ.isCart (𝟙 J) τ'
   have LeqPsiTau : ψ.1 ≫ τ.φ.1 = L.φ.1 := by
     rw [hψ.1]
     apply Category.id_comp
@@ -81,24 +96,100 @@ def weakCartifCartesian {J I : B} {u : J ⟶ I} {X : obj_over (P:=P) I} {τ: lif
   apply hψ.2
   rw [← hψ.1, hψ' , ← LeqPsiTau]
 
-theorem cartesianLiftIsUnique {J I : B} {u : J ⟶ I} {X : obj_over (P:=P) I} (L L' : cartesianLiftOfAlong X u) :
+theorem cartesianLiftIsUnique {J I : B} {u : J ⟶ I} {X : obj_over (P:=P) I} (L  L' : cartesianLiftOfAlong X u) :
   ∃! α : L'.1.Y ≅ L.1.Y , α.hom.1 ≫ L.1.φ.1 = L'.1.φ.1 := by
-    obtain ⟨Y , φ⟩ := L.1
-    obtain ⟨Z , ψ⟩ := L'.1
-    have this := L.2 (𝟙 _)
-    have me := Category.id_comp u
-    have Z' : liftOfAlong X (𝟙 J ≫ u):= by apply transLift ; exact ⟨ Z , by sorry ⟩ ; exact L'.1 -- by rw [me] ; exact L'.1
-    have helper : Z'.Y = Z := by trans L'.1.1 ; sorry ; sorry
-    specialize this Z'
+    obtain ⟨ α , hα  ⟩ := weakCartifCartesian (X:=X) L L'
+    obtain ⟨ β , hβ  ⟩ := weakCartifCartesian (X:=X) L' L
 
-    obtain ⟨ α , hα  ⟩ := this
+    obtain ⟨ρ , hρ⟩ := weakCartifCartesian (X:=X) L' L'.1
+    obtain ⟨ ⟨ Y , φ ⟩ , _⟩   := L
+    obtain ⟨ ⟨ Z , ψ⟩ , _⟩  := L'
+    simp at α
+    simp at β
+    simp at hβ
+    simp at hα
+    simp at hρ
+    have this : (α ≫β  ).1 = α.1 ≫ β.1 := by rfl
 
-    have α : Z ⟶ Y := ⟨ by sorry , by sorry ⟩
-    let α : Z ≅ Y  := ⟨ α , by sorry , by sorry, by sorry ⟩
+    have abh : (α ≫ β).1 ≫ ψ.1 = ψ.1  := by rw [this , Category.assoc , hβ.1 , hα.1]
+    have abh : α ≫β = 𝟙 _ := by
+      let hρ := hρ.2
+      trans ρ
+      apply hρ
+      exact abh
+      symm
+      apply hρ
+      have this : (𝟙 _) ≫ ψ.1 = ψ.1 := by rw [Category.id_comp]
+      exact this
+    have bah : (β ≫α  ).1 ≫ φ.1 = φ.1  := by sorry
+    have bah : β ≫ α= 𝟙 _ := by sorry
 
-    use α
+      -- trans ((𝟙 (L'.1).Y) ≫ (L'.1).φ )
 
-def fibration := ∀ {J I : B} (u : J ⟶ I) (X : obj_over I) , cartesianLiftOfAlong ( P:=P) X u
+    let myiso : Z ≅ Y  := ⟨ α , β , abh, bah ⟩
+
+    -- have h : myiso.hom = α := by sorry
+    use myiso
+    constructor
+    · simp
+      exact hα.1
+    · intro α'  hα'
+      ext
+      apply hα.2
+      exact hα'
+variable  {B : Cat.{v₁ , u₁}}
+
+
+
+instance : CoeDep (Over B) F (F.1 ⥤ B) where
+  coe := F.hom
+
+def fibration (B : Cat.{v₁ , u₁}) := { P : Over B  //
+  ∀ {J I : B} (u : J ⟶ I) (X : obj_over I) ,
+    ∃ φ:  liftOfAlong (P:=P.hom) X u , isCartesian φ }
+
+
+-- variable {𝕏 : Type u₂} {B : Type u₁} [Category.{v₁} B] [Category.{v₂} 𝕏] {P : 𝕏 ⥤ B}
+instance : CoeOut (fibration B) (Over B) := ⟨ fun α ↦ α.1⟩
+
+instance : CoeDep (fibration B) F (F.1.1 ⥤ B) where
+  coe := F.1
+def isCartesianMorphism (P : Over B ) {X Y : P.1}  (φ : Y ⟶ X) : Prop :=
+  isCartesian (P:=P.hom) (morphismToLift φ)
+def cartesianFunctor
+  (P Q : fibration B) := {F : P.1 ⟶ Q.1 //
+    ∀ {X Y : P.1.1} (φ : X ⟶ Y) (_ : isCartesianMorphism P.1 φ) ,
+       isCartesianMorphism Q.1 (F.left.map φ)  }
+scoped infixr:80 " ⥤c   " => cartesianFunctor -- type as \gg
+instance {P Q : fibration B} : CoeOut (P ⥤c Q) (P.1.1 ⥤ Q.1.1) := ⟨fun α ↦ α.1.left⟩
+def overFunctorPreservesFibers {P Q : fibration B} (F : P ⥤c Q) (A : B) :
+  Functor (obj_over (P := P.1.hom) A) (obj_over (P := Q.1.hom) A) where
+    obj := fun X ↦ by
+      use (F : P.1.1 ⥤ Q.1.1).obj X.1
+      trans (P.1.hom).obj X
+      · let FQ : P.1.1 ⟶ B := F.1.1 ≫ Q.1.hom
+        trans FQ.obj X.1 ; rfl ;
+        have this : FQ = P.1.hom := F.1.3 ;
+        rw [this]
+      · exact X.2
+    map := fun {X Y} (f : X ⟶ Y) ↦ by
+      use (F.1.left).map f.1
+      simp
+      let FQ : P.1.1 ⟶ B := F.1.1 ≫ Q.1.hom
+      have this : FQ = P.1.hom := F.1.3 ;
+      trans (FQ.map f.1 ≫ eqToHom _)
+
+
+
+
+    map_id := by sorry
+    map_comp := by sorry
+
+
+
+def cartesianNaturalTrafo {P Q : fibration B}
+  (F G : P ⥤c Q)
+  (η : undFuncMor F.1 ⟶ G) := ∀ {X : P.1.1} , η.app X
 
 
 end FiberedCategories
