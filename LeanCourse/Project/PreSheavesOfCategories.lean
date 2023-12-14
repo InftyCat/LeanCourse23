fe2ff4e3c85b188a31a6030463c815f17a427a3b
Tim Lichtnau <tlichtnau@gmail.com>
2023-12-14 22:51:05 +0100

    PreSheavesOfCategegories

--- LeanCourse/Project/PreSheavesOfCategories.lean
+++ LeanCourse/Project/PreSheavesOfCategories.lean
@@ -56,15 +56,13 @@ lemma natHelper {F G : splitFibration B} (α : F ⥤cs G) (u : J ⟶ I)
       have obs3' : ∀ {Z : ↑(F ↓ I) } ,  (appNat Z).hom.1 = eqToHom ((α.2 u Z).choose) := fun {Z} ↦ by rw [appNat , eqToIso.hom] ; sorry
       have obs1 : (appNat Y).inv.1 ≫ (Cart u ((α / I).obj Y)) = (α%).map (Cart u Y)  := by
         let myiso : (((α / I) ≫ re u).obj Y ).1 ≅ ((re u ≫ (α / J)).obj Y ).1 := undAppNat Y
-        let myIsoInv : myiso.inv = (appNat Y).inv.1 := rfl
+        have myIsoInv : myiso.inv = (appNat Y).inv.1 := rfl
         have this : myiso.hom = (appNat Y).hom.1 := rfl
-        apply (CategoryTheory.Iso.cancel_iso_hom_left (myiso) _ _).1
-        rw [← Category.assoc, ← myIsoInv , eq_whisker (Iso.hom_inv_id myiso) _ , Category.id_comp]
+        rw [← myIsoInv]
+        apply (inv_comp_eq myiso).2
         symm
         rw [this, obs3' (Z:=Y)]
         exact (α.2 u Y).choose_spec
-
-
       have orig : CommSq ((re u).map f).1 (Cart u X) (Cart u Y) f.1 := ⟨ (u ⋆ f).choose_spec.1 ⟩
       have obs2 : m α  J ((re u).map f) ≫ (α %).map (Cart u Y) = (α %).map (Cart u X) ≫ m α I f  :=
          (CategoryTheory.Functor.map_commSq (α %) orig).w
@@ -80,18 +78,18 @@ lemma natHelper {F G : splitFibration B} (α : F ⥤cs G) (u : J ⟶ I)
         _ = ((appNat X).hom.1 ≫ (α %).map (Cart u X)) ≫ m α I f                               :=  by rw [← Category.assoc]
         _ = Cart u ((α / I).obj X) ≫ m α I f                                               :=  (eq_whisker obs3 (m α I f))
         _ = Cart u ((α / I).obj X) ≫ ((α / I).map f).1 := rfl
-
 theorem Naturality {F G : splitFibration B} (α : F ⥤cs G) (u : J ⟶ I) :
-  (α / I) ≫ re u  ≅ re u ≫ (α / J) := by
-    apply NatIso.ofComponents
-    · intro X Y f
+  (α / I) ≫ re u  ≅ re u ≫ (α / J) :=
+    NatIso.ofComponents appNat
+    (by
+      intro X Y f
       apply (Iso.eq_comp_inv (appNat Y)).1
       let αf := (α / I).map f
       symm
       apply (u ⋆ αf ).choose_spec.2
       simp
-      exact natHelper α u f
-    · exact appNat
+      exact natHelper α u f)
+
 
 
 
